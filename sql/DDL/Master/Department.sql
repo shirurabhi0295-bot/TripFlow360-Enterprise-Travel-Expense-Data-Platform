13209816418111b4a9e2ccfbcd856ae3d8430877
Abhishek Shirur <shirurabhi0295@gmail.com>
2026-07-09 20:10:07 +0530

    Creating the Department Table
    Created the first table to start the project

---
+++ sql/DDL/Master/Department.sql
@@ -0,0 +1,26 @@
+USE TripFlow360DB;
+GO
+
+CREATE TABLE Reference.Department
+(
+    DepartmentID INT IDENTITY(1,1) PRIMARY KEY,
+
+    DepartmentCode NVARCHAR(20) NOT NULL,
+
+    DepartmentName NVARCHAR(100) NOT NULL,
+
+    DepartmentType NVARCHAR(50) NOT NULL,
+
+    DepartmentDescription NVARCHAR(255) NULL,
+
+    IsActive BIT NOT NULL DEFAULT 1,
+
+    CreatedDate DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+    CreatedBy NVARCHAR(100) NOT NULL,
+
+    ModifiedDate DATETIME2 NULL,
+
+    ModifiedBy NVARCHAR(100) NULL
+);
+GO
