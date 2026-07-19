919ea215c0cee7ed9b04c706e146418cbf2aced7
Abhishek Shirur <shirurabhi0295@gmail.com>
2026-07-19 13:21:02 +0530

    Create the employee and convert to CSV

--- python/generators/generate_employee.py
+++ python/generators/generate_employee.py
@@ -1,24 +1,47 @@
-# ============================================
-# TripFlow360 - Employee Data Generator
-# Author : Abhishek Biradar
-# ============================================
-
 from faker import Faker
 import pandas as pd
 import random
+from pathlib import Path
 
-# Indian locale
 fake = Faker("en_IN")
 
-# Generate one employee
+employees = []
 
-employee = {
-    "EmployeeCode": "EMP000001",
-    "FirstName": fake.first_name(),
-    "LastName": fake.last_name(),
-    "Gender": random.choice(["Male", "Female"]),
-    "OfficialEmail": fake.email(),
-    "PhoneNumber": fake.phone_number(),
-}
+for i in range (1,11):
 
-print(employee)
+    gender = random.choice(["Male", "Female"])
+
+    if gender == "Male":
+        first_name = fake.first_name_male()
+    else:
+        first_name = fake.first_name_female()    
+    last_name = fake.last_name()
+
+    first_digit = random.choice(["6", "7", "8", "9"])
+
+    remaining_digits = "".join(random.choices("0123456789", k=9))
+
+    phone_number = first_digit + remaining_digits
+
+    employee = {
+        "EmployeeCode": f"EMP{i:06}",
+        "FirstName": first_name,
+        "LastName": last_name,
+        "Gender": gender,
+        "OfficialEmail": f"{first_name.lower()}.{last_name.lower()}@tripflow360.com",
+        "PhoneNumber": phone_number
+    }
+    employees.append(employee)
+
+df = pd.DataFrame(employees)
+
+print(df)
+
+project_root = Path(__file__).resolve().parents[2]
+
+output_path = project_root / "datasets" / "hr" / "employee.csv"
+
+df.to_csv(output_path, index=False)
+
+print("\nEmployee CSV generated successfully!")
+print(f"Location: {output_path}")
