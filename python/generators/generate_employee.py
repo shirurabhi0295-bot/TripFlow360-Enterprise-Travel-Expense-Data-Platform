fca0f72c60c50900c02123e7687aa91952c7c137
Abhishek Shirur <shirurabhi0295@gmail.com>
2026-07-22 08:08:23 +0530

    Phase 3-Generate Employee Using Functions
    Using the Functions to generate employee for better readability and reusability.

--- python/generators/generate_employee.py
+++ python/generators/generate_employee.py
@@ -5,32 +5,67 @@ from pathlib import Path
 
 fake = Faker("en_IN")
 
-employees = []
 
-for i in range (1,11):
-
+def generate_name():
     gender = random.choice(["Male", "Female"])
 
     if gender == "Male":
         first_name = fake.first_name_male()
     else:
-        first_name = fake.first_name_female()    
+        first_name = fake.first_name_female()
+
     last_name = fake.last_name()
 
-    first_digit = random.choice(["6", "7", "8", "9"])
+    return first_name, last_name, gender
 
+
+def generate_phone_number():
+    first_digit = random.choice(["6", "7", "8", "9"])
     remaining_digits = "".join(random.choices("0123456789", k=9))
 
-    phone_number = first_digit + remaining_digits
+    return first_digit + remaining_digits
+
+
+def generate_email(first_name, last_name):
+    return f"{first_name.lower()}.{last_name.lower()}@tripflow360.com"
+
+
+def create_employee(employee_number):
+
+    first_name, last_name, gender = generate_name()
+
+    phone_number = generate_phone_number()
+
+    email = generate_email(first_name, last_name)
 
     employee = {
-        "EmployeeCode": f"EMP{i:06}",
+        "EmployeeCode": f"EMP{employee_number:06}",
         "FirstName": first_name,
         "LastName": last_name,
         "Gender": gender,
-        "OfficialEmail": f"{first_name.lower()}.{last_name.lower()}@tripflow360.com",
+        "OfficialEmail": email,
         "PhoneNumber": phone_number
     }
+
+    return employee
+
+def save_employee_csv(df,output_path):
+    df.to_csv(output_path, index=False)
+    print("\nEmployee CSV generated successfully!")
+
+def load_employee_csv(file_path):
+
+    df = pd.read_csv(file_path)
+
+    return df    
+
+
+# ---------------- Main Program ---------------- #
+
+employees = []
+
+for i in range(1, 11):
+    employee = create_employee(i)
     employees.append(employee)
 
 df = pd.DataFrame(employees)
@@ -41,7 +76,7 @@ project_root = Path(__file__).resolve().parents[2]
 
 output_path = project_root / "datasets" / "hr" / "employee.csv"
 
-df.to_csv(output_path, index=False)
+save_employee_csv(df, output_path)
+employee_df = load_employee_csv(output_path)
 
-print("\nEmployee CSV generated successfully!")
-print(f"Location: {output_path}")
+print(employee_df.head())
